13372c7f105076bbbd04afc75d6307d27b953e1d
noda <>
2022-06-10 12:25:52 +0900

    コメント追加

--- functions/common/connectdb.php
+++ functions/common/connectdb.php
@@ -1,12 +1,17 @@
 <?php
+    /*
+        @return PDOstatement
+        DB接続失敗時はNULL
+
+    */
     function connectDb(){
         try{
             $pdo = new PDO('mysql:host=localhost;dbname=masa', 'root', 'root');
             $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             return $pdo;
         }
-        catch(PDOException $e){
-            return $db=null;
+        catch(Exception $e){
+            return $db = null;
         }
     }
 ?>
